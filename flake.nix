520cbf480035cf4cd635aa5971e5ef7b23305ee1
otf <otf@me.com>
2021-05-31 16:55:43 +0900

    Select defaultPackage in flakes.
    refer to https://github.com/snabblab/snabblab-nixos/blob/master/make-iso.nix

--- flake.nix
+++ flake.nix
@@ -2,29 +2,14 @@
   description = "Installer iso image for Telebox.";
   inputs.nixos.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixos }: {
-    nixosConfigurations = let
-      # Shared base configuration.
-      teleboxBase = {
+    defaultPackage."x86_64-linux" =
+      with (import "${nixos}/nixos/lib/eval-config.nix") {
         system = "x86_64-linux";
         modules = [
-          # Common system modules ...
-        ];
-      };
-    in {
-      teleboxIso = nixos.lib.nixosSystem {
-        inherit (teleboxBase) system;
-        modules = teleboxBase.modules ++ [
           "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         ];
       };
-
-      telebox = nixos.lib.nixosSystem {
-        inherit (teleboxBase) system;
-        modules = teleboxBase.modules ++ [
-          # Modules for installed systems only.
-        ];
-      };
-    };
+      config.system.build.isoImage;
   };
 }
 
