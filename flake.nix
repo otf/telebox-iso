d7204fd03f6f4302f4930026241c47206db88f3e
otf <otf@me.com>
2021-06-02 19:36:06 +0900

    Beep during installation.

--- flake.nix
+++ flake.nix
@@ -7,6 +7,21 @@
         system = "x86_64-linux";
         modules = [
           "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ({ pkgs, lib, ... }: {
+            systemd.services.installation = {
+              description = "Installation";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" "polkit.service" ];
+              script = with pkgs; ''
+                ${kmod}/bin/modprobe pcspkr
+                ${beep}/bin/beep -f 2000;
+                ${beep}/bin/beep -f 1000;
+              '';
+              serviceConfig = {
+                Type = "oneshot";
+              };
+            };
+          })
         ];
       };
       config.system.build.isoImage;
