27ecd2b2610d83c327e04d2486f11c2e185bb373
otf <otf@me.com>
2021-06-03 12:01:15 +0900

    Add a label on the iso image.

--- flake.nix
+++ flake.nix
@@ -2,36 +2,41 @@
   description = "Installer iso image for Telebox.";
   inputs.nixos.url = "github:nixos/nixpkgs/nixos-20.09";
   outputs = { self, nixos }: {
-    defaultPackage."x86_64-linux" =
-      with (import "${nixos}/nixos/lib/eval-config.nix") {
-        system = "x86_64-linux";
-        modules = [
-          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          ({ pkgs, lib, ... }: {
-            systemd.services.installation = {
-              description = "Installation";
-              wantedBy = [ "multi-user.target" ];
-              after = [ "getty.target" "network.target" ];
-              conflicts = [ "getty@tty1.service" ];
-              script = with pkgs; ''
-                ${kmod}/bin/modprobe pcspkr
-                ${beep}/bin/beep -f 2000;
-                ${beep}/bin/beep -f 1000;
-              '';
-              serviceConfig = {
-                Type = "oneshot";
-                RemainAfterExit = "yes";
-                StarndardInput = "tty-force";
-                StarndardOutput = "inherit";
-                StarndardError = "inherit";
-                TTYReset = "yes";
-                TTYVHangup = "yes";
+    defaultPackage."x86_64-linux" = 
+      let 
+        telebox = (import "${nixos}/nixos/lib/eval-config.nix") {
+          system = "x86_64-linux";
+          modules = [
+            "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+            ({ pkgs, lib, ... }: rec {
+              isoImage.isoBaseName = "telebox";
+              isoImage.volumeID = "TELEBOX_ISO";
+
+              systemd.services.installation = {
+                description = "Installation";
+                wantedBy = [ "multi-user.target" ];
+                after = [ "getty.target" "network.target" ];
+                conflicts = [ "getty@tty1.service" ];
+                script = with pkgs; ''
+                  ${kmod}/bin/modprobe pcspkr
+                  ${beep}/bin/beep -f 2000;
+                  ${beep}/bin/beep -f 1000;
+                '';
+                serviceConfig = {
+                  Type = "oneshot";
+                  RemainAfterExit = "yes";
+                  StarndardInput = "tty-force";
+                  StarndardOutput = "inherit";
+                  StarndardError = "inherit";
+                  TTYReset = "yes";
+                  TTYVHangup = "yes";
+                };
               };
-            };
-          })
-        ];
-      };
-      config.system.build.isoImage;
+            })
+          ];
+        };
+      in
+        telebox.config.system.build.isoImage;
   };
 }
 
