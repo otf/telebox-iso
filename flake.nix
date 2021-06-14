3d90dfebbfb706c09477762e637055e4cda549dd
otf <otf@me.com>
2021-06-14 17:19:22 +0900

    Extract a nix that be for installation.

--- flake.nix
+++ flake.nix
@@ -7,62 +7,14 @@
     in {
       defaultPackage.${system} = 
         let 
-          configFile = builtins.toFile "configuration.nix" (builtins.readFile ./modules/configuration.nix);
-          hardwareConfigFile = builtins.toFile "hardware-configuration.nix" (builtins.readFile ./modules/hardware-configuration.nix);
           telebox = (import "${nixpkgs}/nixos/lib/eval-config.nix") {
             inherit system;
             modules = [
               "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-              ({ config, pkgs, lib, ... }: rec {
+              ./modules/installation.nix
+              ({ config, pkgs, lib, ... }: {
                 isoImage.isoBaseName = "telebox";
                 isoImage.volumeID = "TELEBOX_ISO";
-
-                systemd.services.installation = {
-                  description = "Installation";
-                  wantedBy = [ "multi-user.target" ];
-                  after = [ "getty.target" "nscd.service" ];
-                  conflicts = [ "getty@tty1.service" ];
-                  script = with pkgs; ''
-                    set -euxo pipefail
-
-                    # Partitioning
-                    wipefs -fa /dev/nvme0n1
-                    parted -s /dev/nvme0n1 -- mklabel gpt
-                    parted -s /dev/nvme0n1 -- mkpart ESP fat32 1MiB 512MiB
-                    parted -s /dev/nvme0n1 -- set 1 esp on
-                    parted -s /dev/nvme0n1 -- mkpart primary ext4 512MiB 100%
-
-                    # Formatting
-                    mkfs.ext4 -F -L nixos /dev/nvme0n1p2
-                    mkfs.fat -F 32 -n boot /dev/nvme0n1p1
-
-                    # Mount
-                    mount /dev/disk/by-label/nixos /mnt
-                    mkdir -p /mnt/boot
-                    mount /dev/disk/by-label/boot /mnt/boot
-
-                    # Install
-                    mkdir -p /mnt/etc/nixos
-                    cp ${configFile} /mnt/etc/nixos/configuration.nix
-                    cp ${hardwareConfigFile} /mnt/etc/nixos/hardware-configuration.nix
-                    ${telebox.config.system.build.nixos-install}/bin/nixos-install --no-root-passwd
-                    reboot
-                  '';
-                  serviceConfig = {
-                    Type = "oneshot";
-                    RemainAfterExit = "yes";
-                    StarndardInput = "tty-force";
-                    StarndardOutput = "inherit";
-                    StarndardError = "inherit";
-                    TTYReset = "yes";
-                    TTYVHangup = "yes";
-                  };
-                  path = [ "/run/current-system/sw" ];
-                  environment = config.nix.envVars // {
-                    inherit (config.environment.sessionVariables) NIX_PATH;
-                    HOME = "/root";
-                  };
-                };
               })
             ];
           };
